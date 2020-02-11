4d63152415fed8b95bdd51f1b122e12d3777dc46
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-10 20:16:39 -0500

    built a signup form that handles change when user starts typing in a field

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,48 +1,66 @@
 import React, { useState } from "react";
 import Layout from "../core/Layout";
-import { API } from "../config";
 
-const Signup = () => (
-  <Layout title="Signup" description="Signup to start Gettin Crafty!">
-    {API}
-  </Layout>
-);
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          // value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          // value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          // value={password}
+        />
+      </div>
+      {/* <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button> */}
+    </form>
+  );
+
+  return (
+    <Layout
+      title="Signup"
+      description="Signup to start Gettin Crafty!"
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+      {JSON.stringify(values)}
+    </Layout>
+  );
+};
 
 export default Signup;
-
-// const signUpForm = () => (
-//   <form>
-//     <div className="form-group">
-//       <label className="text-muted">Name</label>
-//       <input
-//         onChange={handleChange("name")}
-//         type="text"
-//         className="form-control"
-//         value={name}
-//       />
-//     </div>
-
-//     <div className="form-group">
-//       <label className="text-muted">Email</label>
-//       <input
-//         onChange={handleChange("email")}
-//         type="email"
-//         className="form-control"
-//         value={email}
-//       />
-//     </div>
-
-//     <div className="form-group">
-//       <label className="text-muted">Password</label>
-//       <input
-//         onChange={handleChange("password")}
-//         type="password"
-//         className="form-control"
-//         value={password}
-//       />
-//     </div>
-//     <button onClick={clickSubmit} className="btn btn-primary">
-//       Submit
-//     </button>
-//   </form>
-// );
