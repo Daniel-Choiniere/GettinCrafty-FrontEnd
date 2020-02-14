dc3a4eab7500db41b1b88542698af412dc4521eb
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-14 14:21:14 -0500

    buiulding off the template for our user dashboard, I built a Admin dashboard with different links on sidebar

--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,7 @@ const Routes = () => {
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
           <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+          <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         </Switch>
       </div>
     </BrowserRouter>
