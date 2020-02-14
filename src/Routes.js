65edb3f858511b609bfda61e994921f4dfc78bf6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-14 14:31:28 -0500

    implemented the admin route so you get sent to the correct dashboard when signing in

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,9 @@ import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
 import PrivateRoute from "./auth/PrivateRoute";
+import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
+import AdminDashboard from "./user/AdminDashboard";
 
 const Routes = () => {
   return (
@@ -15,7 +17,11 @@ const Routes = () => {
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
           <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
-          <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+          <AdminRoute
+            path="/admin/dashboard"
+            exact
+            component={AdminDashboard}
+          />
         </Switch>
       </div>
     </BrowserRouter>
