4a809f86d820ba348260d3852bed3ce3e2d51be4
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-14 11:41:49 -0500

    added next callback ontop authenticate method so signup doesnt get hung up on loading screen

--- src/auth/index.js
+++ src/auth/index.js
@@ -37,5 +37,8 @@ export const signin = user => {
 export const authenticate = (data, next) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("jwt", JSON.stringify(data));
+    next();
   }
 };
+
+export const signout = next => {};
