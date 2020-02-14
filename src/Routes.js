dd20da5b079d0e60593830a837e97b6fc01a4757
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-14 17:42:27 -0500

    built a component to handle adim creating a category

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import PrivateRoute from "./auth/PrivateRoute";
 import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
 import AdminDashboard from "./user/AdminDashboard";
+import AddCategory from "./admin/AddCategory";
 
 const Routes = () => {
   return (
@@ -22,6 +23,7 @@ const Routes = () => {
             exact
             component={AdminDashboard}
           />
+          <AdminRoute path="/create/category" exact component={AddCategory} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/admin/AddCategory.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+
+const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  // destructure user and token
+  const { user, token } = isAuthenticated();
+
+  const handleChange = e => {
+    setError("");
+    setName(e.tatget.value);
+  };
+
+  const clickSubmit = e => {
+    e.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    //   make request to api to create category
+  };
+
+  const newCategoryForm = () => (
+    <form onSubmit={clickSubmit}>
+      <div className="form-group">
+        <label className="text-muted">Category Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          value={name}
+          autoFocus
+        />
+        <button className="btn btn-outline-primary">Create Category</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout
+      title="Add a new category"
+      description={`Remember to smile ${name}!`}
+    >
+      <div className="row">
+        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AddCategory;
