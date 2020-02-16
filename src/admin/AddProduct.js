abb2fe9c670cad66b2cab57c3cb11332591f4d11
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-16 17:47:35 -0500

    built the form for adding a new product

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -1,11 +1,127 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-import { createProduct } from "./apiAdmin";
-import { Link } from "react-router-dom";
+// import { createProduct } from "./apiAdmin";
+// import { Link } from "react-router-dom";
 
 const AddProduct = () => {
   const { user, token } = isAuthenticated();
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    catagories: "",
+    category: "",
+    shipping: "",
+    quantitiy: "",
+    photo: "",
+    loading: false,
+    error: "",
+    createdProduct: "",
+    redirectToProfile: false,
+    formData: ""
+  });
+
+  const {
+    name,
+    description,
+    price,
+    catagories,
+    category,
+    shipping,
+    quantity,
+    loading,
+    error,
+    createdProduct,
+    redirectToProfile,
+    formData
+  } = values;
+
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData() });
+  }, []);
+
+  const handleChange = name => event => {
+    //   if name is 'photo' grap the img file, if not grab all the data
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+    // will set the form data for each field i.e. name, description, price, etc.
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
+
+  const clickSubmit = e => {};
+
+  const newProductForm = () => (
+    <form className="mb-3" onSubmit={clickSubmit}>
+      <h4>Post Photo</h4>
+      <div className="form-group">
+        <label className="btn, btn-secondary">
+          <input
+            onChange={handleChange("photo")}
+            type="file"
+            name="photo"
+            accept="image/*"
+          />
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Description</label>
+        <textarea
+          onChange={handleChange("description")}
+          className="form-control"
+          value={description}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Price</label>
+        <input
+          onChange={handleChange("price")}
+          type="number"
+          className="form-control"
+          value={price}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Category</label>
+        <select onChange={handleChange("category")} className="form-control">
+          <option value="5e3f3a813f472d09854d675b">T-Shirts</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Shipping</label>
+        <select onChange={handleChange("shipping")} className="form-control">
+          <option value="0">No</option>
+          <option value="1">Yes</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Quantity</label>
+        <input
+          onChange={handleChange("quantity")}
+          type="number"
+          className="form-control"
+          value={quantity}
+        />
+      </div>
+
+      <button className="btn btn-outline-primary">Create Product</button>
+    </form>
+  );
 
   return (
     <Layout
@@ -13,7 +129,7 @@ const AddProduct = () => {
       description={`Remember to smile ${user.name}!`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2"></div>
+        <div className="col-md-8 offset-md-2">{newProductForm()}</div>
       </div>
     </Layout>
   );
