91bf6cefd63c5441f840f26f4cd79c4d393ee565
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-03 20:36:57 -0500

    handling the catagories toggle, displaying correct ids in console

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Checkbox = ({ catagories }) => {
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = c => () => {
+    const currentChecked = checked.indexOf(c);
+    const newCheckedCategoryId = [...checked];
+
+    if (currentChecked === -1) {
+      newCheckedCategoryId.push(c);
+    } else {
+      newCheckedCategoryId.splice(currentChecked, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  };
+
   return catagories.map((c, i) => (
     <li key={i} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input
+        onChange={handleToggle(c._id)}
+        value={checked.indexOf(c._id === -1)}
+        type="checkbox"
+        className="form-check-input"
+      />
       <label className="form-check-label">{c.name}</label>
     </li>
   ));
