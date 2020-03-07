20bd9ba7253ba042ef6c06dd726a1cd9882bbf23
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-07 18:07:56 -0500

    added the category, date added, and if in stock or not... all dynamic values from db... also added some basic stlyling

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
 // takes in prop showViewProductButton from Single Product component to not show card buttons
 const Card = ({ product, showViewProductButton = true }) => {
@@ -15,17 +16,38 @@ const Card = ({ product, showViewProductButton = true }) => {
       )
     );
   };
+
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-primary mt-2 mb-2">Add To Cart</button>
+    );
+  };
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock</span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out Of Stock</span>
+    );
+  };
+
   return (
     <div className="card">
-      <div className="card-header">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
-        <p>{product.description.substring(0, 100)}</p>
-        <p>${product.price}</p>
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="black-10">${product.price}</p>
+        <p className="black-9">
+          Category: {product.category && product.category.name}
+        </p>
+        <p className="black-8">
+          Product added to shop {moment(product.createdAt).fromNow()}
+        </p>
+        {showStock(product.quantity)}
+        <br />
         {showViewButton(showViewProductButton)}
-        <button className="btn btn-outline-primary mt-2 mb-2">
-          Add To Cart
-        </button>
+        {showAddToCartButton()}
       </div>
     </div>
   );
