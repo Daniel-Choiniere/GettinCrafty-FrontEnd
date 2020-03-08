cf83b66d63ffef4c87920501076c4355f0279bc4
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-08 12:45:36 -0400

    i have created a cart component that fetches the users items from localstorage and displays them

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
+import Cart from "./core/Cart";
 import PrivateRoute from "./auth/PrivateRoute";
 import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
@@ -19,6 +20,7 @@ const Routes = () => {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/shop" exact component={Shop} />
+          <Route path="/cart" exact component={Cart} />
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
           <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
