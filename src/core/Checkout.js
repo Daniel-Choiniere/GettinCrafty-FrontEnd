250593f0ded011fecaf0b7d3d8ac9a41e08f71ed
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-13 19:07:09 -0400

    saving the customers delivery address to the db

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -59,6 +59,8 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
     );
   };
 
+  let deliveryAddress = data.address;
+
   const buy = () => {
     setData({ loading: true });
     // send nonce to your server
@@ -80,7 +82,7 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
               products: products,
               transaction_id: response.transaction.id,
               amount: response.transaction.amount,
-              address: data.address
+              address: deliveryAddress
             };
 
             createOrder(userId, token, createOrderData);
