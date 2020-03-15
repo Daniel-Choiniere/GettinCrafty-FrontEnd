59650af27311710619992d5a554bdd62f3e97413
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-15 09:16:38 -0400

    succesfully allowing user to update their profile name from a form on the front end. Once form is submitted it updates the user in db

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { read, update, updateUser } from "./apiUser";
 
 const Profile = props => {
@@ -32,7 +32,74 @@ const Profile = props => {
 
   useEffect(() => {
     init(props.match.params.userId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleChange = name => e => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const clickSubmit = e => {
+    e.preventDefault();
+    update(props.match.params.userId, token, { name, email, password }).then(
+      data => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          updateUser(data, () => {
+            setValues({
+              ...values,
+              name: data.name,
+              email: data.email,
+              success: true
+            });
+          });
+        }
+      }
+    );
+  };
+
+  const redirectUser = success => {
+    if (success) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const profileUpdate = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          onChange={handleChange("name")}
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          type="text"
+          onChange={handleChange("email")}
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="text"
+          onChange={handleChange("password")}
+          className="form-control"
+          value={password}
+        />
+      </div>
+
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 
   return (
     <Layout
@@ -41,7 +108,8 @@ const Profile = props => {
       className="container-fluid"
     >
       <h2 className="mb-4">Profile Update</h2>
-      {JSON.stringify(values)}
+      {profileUpdate(name, email, password)}
+      {redirectUser(success)}
     </Layout>
   );
 };
