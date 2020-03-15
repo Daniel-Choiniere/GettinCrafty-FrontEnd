d723f47155eae2a4713816a9d2522d4f304d20e3
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-15 08:17:36 -0400

    we access the user id from the props to be able to get all the user data we need

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { read, update, updateUser } from "./apiUser";
 
-const Profile = () => {
-  return <h2>PROFILE</h2>;
+const Profile = props => {
+  const [values, setValues] = useState({
+    name: " ",
+    email: " ",
+    password: " ",
+    error: false,
+    success: false
+  });
+
+  const { token } = isAuthenticated();
+  const { name, email, password, error, success } = values;
+
+  const init = userId => {
+    read(userId, token).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          email: data.email
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(props.match.params.userId);
+  }, []);
+
+  return (
+    <Layout
+      title="Profile"
+      description="Update Your Profile"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">Profile Update</h2>
+      {JSON.stringify(values)}
+    </Layout>
+  );
 };
 
 export default Profile;
