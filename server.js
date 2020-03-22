4b9f67c52dd67d80b7fb0faf62aa7060928b9b4c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-22 13:08:23 -0400

    integrated back end into front end directory

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const compression = require("compression");
 const path = require("path");
 const app = express();
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, "build")));
+// Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  // Set the static folder
+  app.use(express.static("client/GettinCrafty/build"));
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "GettinCrafty", "build", "index.html")
+    );
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 
