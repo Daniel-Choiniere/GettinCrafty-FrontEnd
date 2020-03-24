4eda8a0db5aada2d154659fa3bcefa993a00a676
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-24 19:03:14 -0400

    hopefully got app back to working order

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 const express = require("express");
-// const compression = require("compression");
+const compression = require("compression");
 const path = require("path");
 const app = express();
 
-// app.use(compression());
-// app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(compression());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
-
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
