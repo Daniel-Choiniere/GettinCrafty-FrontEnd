5b409f631bbe782777683ada481b52c8cd240279
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-25 19:40:09 -0400

    fn deployin

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const compression = require("compression");
 const path = require("path");
 const app = express();
 
-// Serve static assests if in production
 if (process.env.NODE_ENV === "production") {
-  // Set the static folder
-  app.use(express.static("client/build"));
+  // Set static folder
+  app.use(express.static("build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
