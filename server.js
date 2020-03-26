4da67440fba98944e2330eb41a06d4e2dda00293
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-26 00:08:59 -0400

    reverted back to34d43412b543ba50460517f8b987a27a3b7b0331

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const app = express();
+
+// Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  // Set the static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
