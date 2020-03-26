5a24315f223bc0a072abc5d0a8b04bc0f1b76d53
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-25 22:59:07 -0400

    deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const compression = require("compression");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
@@ -11,6 +12,18 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+  console.log(`DB Connection error: ${err.message}`);
+});
 
 const PORT = process.env.PORT || 3000;
 
